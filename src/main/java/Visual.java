7614a40e948b6aff17b5ee44c107a91694220831
Olaia98 <olaiazenigaonaindia@gmail.com>
2025-03-13 14:17:33 +0100

    comentario IP

--- src/main/java/Visual.java
+++ src/main/java/Visual.java
@@ -9,8 +9,9 @@ import java.util.concurrent.ExecutionException;
 
 
 public class Visual {
-//hola
+
     private static final Logger logger = LoggerFactory.getLogger(Visual.class);
+    //ipconfig
     private static final String CONNECTION_STRING = "s7://192.168.2.12:102";
 
     public static void main(String[] args) {
